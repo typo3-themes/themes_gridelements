c78d72d9d7cdcb43d7f67e780038d4a41cf88e4c
Kay Strobach <github@kay-strobach.de>
2014-03-16 14:15:20 +0100

    Fix backpath

--- Classes/Hooks/PreHeaderRenderHook.php
+++ Classes/Hooks/PreHeaderRenderHook.php
@@ -1,11 +1,14 @@
 <?php
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class Tx_ThemesGridelements_Hooks_PreHeaderRenderHook {
 	function main($arg) {
 		/** @var $pagerenderer t3lib_PageRenderer */
 		$pagerenderer = $arg['pageRenderer'];
-		$pagerenderer->addCssFile(ExtensionManagementUtility::extRelPath('themes_gridelements') . 'Resources/Public/Stylesheets/t3skin_overrides.css');
+		$pagerenderer->addCssFile(
+			$pagerenderer->backPath . ExtensionManagementUtility::extRelPath('themes_gridelements') . 'Resources/Public/Stylesheets/t3skin_overrides.css'
+		);
 	}
 }
