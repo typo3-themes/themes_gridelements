2dd77646f8cb54417241ad47d8ae56a8fea088af
Thomas Deuling <info@coding.ms>
2014-04-30 15:11:46 +0200

    typoscript include fix

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,6 +1,6 @@
 page = PAGE
 <INCLUDE_TYPOSCRIPT: source="DIR:EXT:themes_gridelements/Configuration/Elements">
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:themes_gridelements/Configuration/TypoScript/Library" extension="setupts">
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:themes_gridelements/Configuration/TypoScript/Library" extensions="setupts">
 
 
 includeLibs.t3jquery = EXT:t3jquery/class.tx_t3jquery.php
