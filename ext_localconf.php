5800426e430a6c6449b479ad62506cf49adf8c4e
Thomas Deuling <info@coding.ms>
2014-09-09 14:00:20 +0200

    Add some theme constants

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 
-if (TYPO3_MODE === 'BE') {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['BackendLayoutFileProvider']['dir'][]
-		=  'EXT:themes_gridelements/Resources/Private/BackendLayouts/';
-
-}
+//if (TYPO3_MODE === 'BE') {
+//	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['BackendLayoutFileProvider']['dir'][]
+//		=  'EXT:themes_gridelements/Configuration/BackendLayouts/';
+//
+//}
