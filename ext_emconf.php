c2bdb146e484bcf284cbb1336a608fb0bda417dc
Jo Hasenau <jo.hasenau@external.telekom.de>
2015-05-06 20:57:58 +0200

    [TASK] Moved button content from themes_gridelements to themes
    Since buttons and icons are basic features and no grids
    these must be available for every theme even without using
    gridelements or extensions based on grids

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => '',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.3.0',
+	'version' => '0.4.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
 			'typo3' => '6.1.99-6.2.99',
-			'themes' => '2.1.0-2.3.99',
+			'themes' => '2.4.0-2.4.99',
 			'gridelements' => '3.2.0-3.2.99',
 		),
 		'conflicts' => array(
--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,5 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_themesgridelements_buttoncontent');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    array(
-        'LLL:EXT:themes_gridelements/Resources/Private/Language/ButtonContent.xlf:tt_content.CType_pi1',
-        $_EXTKEY . '_buttoncontent_pi1',
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'buttoncontent_icon.gif'
-    ),
-    'CType'
-);
-
 // Add Rootline fields for default meta-tags
 $TYPO3_CONF_VARS['FE']['addRootLineFields'] = 'abstract,keywords,description,author,author_email,' . $TYPO3_CONF_VARS['FE']['addRootLineFields'];
 $TYPO3_CONF_VARS['FE']['addRootLineFields'] = implode(',', array_unique(explode(',', $TYPO3_CONF_VARS['FE']['addRootLineFields'])));
