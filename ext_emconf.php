ea99d4d687ebb2db165a29bf863847a21413b6d0
Kay Strobach <info@kay-strobach.de>
2015-11-26 10:05:06 +0100

    [TASK] raise compat to 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-0.0.0',
-			'typo3' => '6.1.99-6.2.99',
+			'typo3' => '6.1.99-7.6.99',
 			'themes' => '2.5.0-2.5.99',
 			'gridelements' => '3.2.0-3.2.99',
 		),
