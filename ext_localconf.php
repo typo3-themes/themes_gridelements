5695f86d654ab229ddb32d402ca9a10ea545e382
Thomas Deuling <info@coding.ms>
2017-03-25 21:06:00 +0100

    [TASK] Cleanup unused files

--- ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//if (TYPO3_MODE === 'BE') {
-//	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['BackendLayoutFileProvider']['dir'][]
-//		=  'EXT:themes_gridelements/Configuration/BackendLayouts/';
-//
-//}
