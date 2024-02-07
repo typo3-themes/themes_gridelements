fa0cc40a42da5d72c70e1a8568b6ae46c490bb16
Thomas Deuling <tdeuling@coding.ms>
2024-02-07 21:18:06 +0100

    [TASK] Migration for TYPO3 12

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,52 +1,12 @@
 <?php
 
 // Show media field for special gridlayouts
-$GLOBALS['TCA']['tt_content']['columns']['media']['displayCond'] = array(
-	'OR' => array(
-		'FIELD:CType:!=:gridelements_pi1',
-		'FIELD:tx_gridelements_backend_layout:IN:container,row,singleColumn,singleColumnHorizontal,carousel',
-	)
-);
+$GLOBALS['TCA']['tt_content']['columns']['media']['displayCond'] = ['OR' => ['FIELD:CType:!=:gridelements_pi1', 'FIELD:tx_gridelements_backend_layout:IN:container,row,singleColumn,singleColumnHorizontal,carousel']];
 
 
 // Enforce equal column height
 // Column settings
-$tempColumn = array(
-	'tx_themes_enforceequalcolumnheight' => array(
-		'displayCond' => array(
-			'AND' => array(
-				'FIELD:CType:=:gridelements_pi1',
-				'OR' => array(
-					'FIELD:tx_gridelements_backend_layout:=:row',
-					'FIELD:tx_gridelements_backend_layout:=:column',
-				),
-			),
-		),
-		'exclude' => 1,
-		'label' => 'LLL:EXT:themes/Resources/Private/Language/locallang.xlf:enforce_equal_column_height',
-		'config' => array(
-            'type' => 'input',
-            'renderType' => 'ThemesContentEnforceEqualColumnHeight'
-		)
-	),
-	'tx_themes_columnsettings' => array(
-		'displayCond' => array(
-			'AND' => array(
-				'FIELD:CType:=:gridelements_pi1',
-				'OR' => array(
-					'FIELD:tx_gridelements_backend_layout:=:singleColumn',
-					'FIELD:tx_gridelements_backend_layout:=:singleColumnHorizontal',
-				),
-			),
-		),
-		'exclude' => 1,
-		'label' => 'LLL:EXT:themes/Resources/Private/Language/locallang.xlf:column_settings',
-		'config' => array(
-			'type' => 'input',
-            'renderType' => 'ThemesContentColumnSettings'
-		)
-	),
-);
+$tempColumn = ['tx_themes_enforceequalcolumnheight' => ['displayCond' => ['AND' => ['FIELD:CType:=:gridelements_pi1', 'OR' => ['FIELD:tx_gridelements_backend_layout:=:row', 'FIELD:tx_gridelements_backend_layout:=:column']]], 'exclude' => 1, 'label' => 'LLL:EXT:themes/Resources/Private/Language/locallang.xlf:enforce_equal_column_height', 'config' => ['type' => 'input', 'renderType' => 'ThemesContentEnforceEqualColumnHeight']], 'tx_themes_columnsettings' => ['displayCond' => ['AND' => ['FIELD:CType:=:gridelements_pi1', 'OR' => ['FIELD:tx_gridelements_backend_layout:=:singleColumn', 'FIELD:tx_gridelements_backend_layout:=:singleColumnHorizontal']]], 'exclude' => 1, 'label' => 'LLL:EXT:themes/Resources/Private/Language/locallang.xlf:column_settings', 'config' => ['type' => 'input', 'renderType' => 'ThemesContentColumnSettings']]];
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumn);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'media', 'gridelements_pi1', 'after:section_frame');
