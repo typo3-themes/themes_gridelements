6353f1140a90052653880eb77c789fbfcebbad87
Kay Strobach <info@kay-strobach.de>
2026-03-11 13:14:39 +0100

    [FEATURE] Update flexforms for modern typo3
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

---
+++ Configuration/Icons.php
@@ -0,0 +1,176 @@
+<?php
+
+return [
+    'themes-backendlayout-content' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/Content.svg',
+    ],
+    'themes-backendlayout-contentempty' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentEmpty.svg',
+    ],
+    'themes-backendlayout-contentstartsite' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentStartsite.svg',
+    ],
+    'themes-backendlayout-contentmenu' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentMenu.svg',
+    ],
+    'themes-backendlayout-contentmenusidebar' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentMenuSidebar.svg',
+    ],
+    'themes-backendlayout-contentsidebar' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentSidebar.svg',
+    ],
+    'themes-backendlayout-contentsidebarmenu' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentSidebarMenu.svg',
+    ],
+    'themes-backendlayout-contentspecial' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/ContentSpecial.svg',
+    ],
+    'themes-backendlayout-default' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/Default.svg',
+    ],
+    'themes-backendlayout-menucontent' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/MenuContent.svg',
+    ],
+    'themes-backendlayout-menucontentsidebar' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/MenuContentSidebar.svg',
+    ],
+    'themes-backendlayout-menusidebarcontent' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/MenuSidebarContent.svg',
+    ],
+    'themes-backendlayout-sidebarcontent' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/SidebarContent.svg',
+    ],
+    'themes-backendlayout-sidebarcontentmenu' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/SidebarContentMenu.svg',
+    ],
+    'themes-backendlayout-sidebarmenucontent' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/BackendLayouts/SidebarMenuContent.svg',
+    ],
+    'new-content-el-2-column' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_2_Column.svg',
+    ],
+    'new-content-el-3-column' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_3_Column.svg',
+    ],
+    'new-content-el-4-column' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_4_Column.svg',
+    ],
+    'new-content-el-accordion' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_Accordion.svg',
+    ],
+    'new-content-el-carousel' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_Carousel.svg',
+    ],
+    'new-content-el-container' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_Container.svg',
+    ],
+    'new-content-el-row' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_Row.svg',
+    ],
+    'new-content-el-singlecolumnheaderfooter' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_SingleColumnHeaderFooter.svg',
+    ],
+    'new-content-el-singlecolumnhorizontal' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_SingleColumnHorizontal.svg',
+    ],
+    'new-content-el-singlecolumnvertical' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_SingleColumnVertical.svg',
+    ],
+    'new-content-el-tab' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/new_content_el_Tab.svg',
+    ],
+    'content-el-2-column-3-9' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/2-Column/2col_3-9.svg',
+    ],
+    'content-el-2-column-4-8' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/2-Column/2col_4-8.svg',
+    ],
+    'content-el-2-column-6-6' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/2-Column/2col_6-6.svg',
+    ],
+    'content-el-2-column-8-4' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/2-Column/2col_8-4.svg',
+    ],
+    'content-el-2-column-9-3' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/2-Column/2col_9-3.svg',
+    ],
+    'content-el-2-column-12-12' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/2-Column/2col_12-12.svg',
+    ],
+    'content-el-3-column-3-3-6' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_3-3-6.svg',
+    ],
+    'content-el-3-column-3-6-3' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_3-6-3.svg',
+    ],
+    'content-el-3-column-4-4-4' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_4-4-4.svg',
+    ],
+    'content-el-3-column-6-3-3' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_6-3-3.svg',
+    ],
+    'content-el-3-column-6-6-12' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_6-6-12.svg',
+    ],
+    'content-el-3-column-6-12-12' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_6-12-12.svg',
+    ],
+    'content-el-3-column-12-12-12' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/3-Column/3col_12-12-12.svg',
+    ],
+    'content-el-4-column-3-3-3-3' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/4-Column/4col_3-3-3-3.svg',
+    ],
+    'content-el-4-column-6-6-6-6' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/4-Column/4col_6-6-6-6.svg',
+    ],
+    'content-el-4-column-12-12-12-12' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/4-Column/4col_12-12-12-12.svg',
+    ],
+    'content-el-column-placeholder' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:themes_gridelements/Resources/Public/Icons/GridElements/Column/content_el_placeholder.svg',
+    ],
+];
